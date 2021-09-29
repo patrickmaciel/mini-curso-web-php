9e2880cf49b15ed34caffb9a0c2d00a6fda13533
Patrick Maciel <patrickmaciel.info@gmail.com>
2021-09-29 19:50:30 -0300

    javascript files

---
+++ curso-javascript/aula-js-002.js
@@ -0,0 +1,47 @@
+var primeiroNome = 'Patrick'; // string
+var sobrenome = 'Maciel'; // string
+var idade = 32; // integer - int
+var sexo = 'm'; // string - char
+var estadoCivil = 'casado'; // string
+var temVeiculo = true; // boolean
+var salario = 15000.50; // decimal - float - double
+var comidasFavoritas = ['pizza', 'lasanha', 'macarrao', 'strogonoff'];
+var endereco = {
+    rua: 'Rua dos Bobos',
+    numero: 123,
+    complemento: 'casa do fundo',
+    bairro: 'Centro',
+    cidade: 'Sao Paulo',
+    estado: 'SP'
+};
+
+var primeiroNome = prompt('Olá. Qual seu primeiro nome?');
+console.log(primeiroNome);
+
+var sobrenome = prompt('Qual seu sobrenome?');
+console.log(sobrenome);
+
+var idade = prompt('Qual sua idade?');
+console.log(idade);
+
+var sexo = prompt('Qual seu sexo?');
+console.log(sexo);
+
+var estadoCivil = prompt('Qual seu estado civil?');
+console.log(estadoCivil);
+
+var temVeiculo = prompt('Você tem veículo?');
+console.log(temVeiculo);
+
+var salario = prompt('Qual seu salario?');
+console.log(salario);
+
+alert('Fim do programa');
+
+/*
+var nomeDoObjeto = {
+    atributo1: valor1,
+    atributo2: valor2,
+    atributo3: valor3
+};
+*/
